e9005d952191718028f3808e6d2b4549f679d9a4
Harsha A K <harshaak2003@gmail.com>
2025-01-06 23:34:14 +0530

    added server link

--- Frontend/src/config.tsx
+++ Frontend/src/config.tsx
@@ -1,3 +1,3 @@
 // export const backend_url = "https://pharmacist-tracking-system.onrender.com";
-// export const backend_url = "https://pharmacist-tracking-system-0001.onrender.com";
-export const backend_url = "http://localhost:3000";
+export const backend_url = "https://pharmacist-tracking-system-0001.onrender.com";
+// export const backend_url = "http://localhost:3000";
